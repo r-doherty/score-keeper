1f2e4124667eef8f91628cce3651fd78a45fab76
unknown <rjd.atx@gmail.com>
2021-08-24 17:59:23 -0500

    Bulma styling and initial logic refactoring

--- app.js
+++ app.js
@@ -9,42 +9,45 @@ const playerTwoScore = document.querySelector(`#playerTwoScore`);
 // Misc. elements for the button to reset the score, as well
 // as the selection field to choose a score to play to.
 const resetButton = document.querySelector(`#resetButton`);
-const scoreToWin = document.querySelector(`#scoreSelector`);
+const scoreSelector = document.querySelector(`#scoreSelector`);
 
 playerOneButton.addEventListener(`click`, () => {
-	let score = parseFloat(playerOneScore.innerHTML) + 1;
-	if (score <= scoreToWin.value) {
-		playerOneScore.innerHTML = `${score}`;
+	let score = parseInt(playerOneScore.textContent) + 1;
+	if (score <= scoreSelector.value) {
+		playerOneScore.textContent = `${score}`;
 	}
-	if (score == scoreToWin.value) {
-		playerOneScore.style.color = "green";
-		playerTwoScore.style.color = "red";
+	if (score == scoreSelector.value) {
+		playerOneScore.classList.add(`has-text-success`);
+		playerTwoScore.classList.add(`has-text-danger`);
 		playerOneButton.disabled = true;
 		playerTwoButton.disabled = true;
-		scoreToWin.disabled = true;
 	}
 });
 
 playerTwoButton.addEventListener(`click`, () => {
-	let score = parseFloat(playerTwoScore.innerHTML) + 1;
-	if (score <= scoreToWin.value) {
-		playerTwoScore.innerHTML = `${score}`;
+	let score = parseInt(playerTwoScore.textContent) + 1;
+	if (score <= scoreSelector.value) {
+		playerTwoScore.textContent = `${score}`;
 	}
-	if (score == scoreToWin.value) {
-		playerOneScore.style.color = "red";
-		playerTwoScore.style.color = "green";
+	if (score == scoreSelector.value) {
+		playerOneScore.classList.add(`has-text-danger`);
+		playerTwoScore.classList.add(`has-text-success`);
 		playerOneButton.disabled = true;
 		playerTwoButton.disabled = true;
-		scoreToWin.disabled = true;
 	}
 });
 
-resetButton.addEventListener(`click`, () => {
-	playerOneScore.innerHTML = 0;
-	playerTwoScore.innerHTML = 0;
-	playerOneScore.style.color = "black";
-	playerTwoScore.style.color = "black";
+resetButton.addEventListener(`click`, reset);
+
+scoreSelector.addEventListener(`change`, () => {
+	reset();
+});
+
+function reset() {
+	playerOneScore.textContent = 0;
+	playerTwoScore.textContent = 0;
+	playerOneScore.classList.remove(`has-text-danger`, `has-text-success`);
+	playerTwoScore.classList.remove(`has-text-danger`, `has-text-success`);
 	playerTwoButton.disabled = false;
 	playerOneButton.disabled = false;
-	scoreToWin.disabled = false;
-});
+}
